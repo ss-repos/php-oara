7d7abfa5f5e7cdfe69f3ce286a824e3a89e4561a
brightnl <bsi01atatl>
2023-07-25 13:28:11 +0200

    Update Webgains to use REST API instead of SOAP

--- Oara/Network/Publisher/WebGains.php
+++ Oara/Network/Publisher/WebGains.php
@@ -10,141 +10,132 @@ namespace Oara\Network\Publisher;
  */
 class Webgains extends \Oara\Network
 {
-	private $_apiClient = null, $username, $password, $campaign_ids;
+	private $publisher_id, $api_token;
 
-	public function login($credentials)
-	{
-		$this->username = $credentials['user'];
-		$this->password = $credentials['password'];
+	public function login($credentials)	{
 
-		$this->campaign_ids = $credentials['program_ids'];
+		$this->api_token = $credentials['api_token'];
+		$this->publisher_id = $credentials['publisher_id'];
 
-		$wsdlUrl = 'https://ws.webgains.com/aws.php';
-		//Setting the client.
-		$this->_apiClient = new \SoapClient($wsdlUrl, array(
-			'encoding' => 'UTF-8',
-			'compression' => SOAP_COMPRESSION_ACCEPT | SOAP_COMPRESSION_GZIP | SOAP_COMPRESSION_DEFLATE,
-			'soap_version' => SOAP_1_1,
-			'keep_alive' => false
-		));
+		return true;
 
-
-		$campaign_id = $this->campaign_ids[0];
-
-		$result = $this->_apiClient->getAccountBalance($this->username, $this->password, $campaign_id);
-		return empty($result);
-	}
-
-	/**
-	 * @return array
-	 */
-	public function getNeededCredentials()
-	{
-		$credentials = array();
-
-		$parameter = array();
-		$parameter["description"] = "User Log in";
-		$parameter["required"] = true;
-		$parameter["name"] = "User";
-		$credentials["user"] = $parameter;
-
-		$parameter = array();
-		$parameter["description"] = "Password to Log in";
-		$parameter["required"] = true;
-		$parameter["name"] = "Password";
-		$credentials["password"] = $parameter;
-
-		return $credentials;
 	}
 
 	/**
 	 * @return bool
 	 */
-	public function checkConnection()
-	{
-		$connection = true;
-		return $connection;
+	public function checkConnection() {
+		return true;
 	}
+
 
 	/**
 	 * @return array
 	 */
-	public function getMerchantList()
-	{
+	public function getMerchantList() {
 		$merchants = array();
 
-		foreach ($this->campaign_ids as $campaign_id) {
+		$url = 'https://platform-api.webgains.com/merchants/programs?filters[program_statuses][]=1&size=250';
 
-			$campaignsList = $this->_apiClient->getProgramsWithMembershipStatus($this->username, $this->password, $campaign_id);
-
-			foreach ($campaignsList as $campaign) {
-				if (!isset($merchants[$campaign->programName]) && $campaign->programMembershipStatusCode == 10) {
-					$obj = Array();
-					$obj['cid'] = $campaign->programID;
-					$obj['name'] = $campaign->programName;
-					$obj['url'] = $campaign->programURL;
-					$merchants[$campaign->programName] = $obj;
-				}
+		$response = self::apiCall($url);
+		if (!empty($response['data'])) {
+			foreach ($response['data'] as $advertiser) {
+				$obj = [];
+				$obj['cid'] = (int)$advertiser['id'];
+				$obj['name'] = (string)$advertiser['name'];
+				$merchants[] = $obj;
 			}
 		}
-
 		return $merchants;
 	}
+
 
 	/**
 	 * @param null $merchantList
 	 * @param \DateTime|null $dStartDate
 	 * @param \DateTime|null $dEndDate
 	 * @return array
+	 * @throws \Exception
 	 */
-	public function getTransactionList($merchantList = null, \DateTime $dStartDate = null, \DateTime $dEndDate = null)
-	{
+	public function getTransactionList($merchantList = null, \DateTime $dStartDate = null, \DateTime $dEndDate = null): array {
 
+		$totalTransactions = [];
+		$page_size = 250; // API max number results is 250
 
-		$totalTransactions = array();
-		$merchantIdList = \Oara\Utilities::getMerchantIdMapFromMerchantList($merchantList);
+		$restUrl = 'https://platform-api.webgains.com/publishers/'.$this->publisher_id.'/reports/transactions?filters[start_date]=' . $dStartDate->getTimestamp() . '&filters[end_date]=' . $dEndDate->getTimestamp() . '&size='.$page_size;
+		$offset = 0;
 
-		foreach ($this->campaign_ids as $campaign_id) {
+		$page = 1;
 
-			$start_date = $dStartDate->format('Y-m-d');
-			$end_date = $dEndDate->add(new \DateInterval('P1D'))->format('Y-m-d');
+		do { // loop over pages
+			$url = $restUrl . '&page=' . $page;
 
-			$transactions = $this->_apiClient->getFullEarningsWithCurrency($start_date, $end_date, $campaign_id, $this->username, $this->password);
+			$result = self::apiCall($url);
 
-			foreach ($transactions as $transaction) {
-				if ($merchantList == null || isset($merchantIdList[(int)$transaction->programID])) {
-					$object = array();
+			if (!empty($result['data'])) {
+				foreach ($result['data'] as $item) {
 
-					$object['unique_id'] = $transaction->transactionID;
+					$transaction = [];
+					$transaction ['unique_id'] = $item['id'];
+					$transaction['merchantId'] = $item['program']['id'];
+					$date = new \DateTime();
+					$date->setTimestamp($item['date']);
+					$transaction['date'] = $date->format("Y-m-d H:i:s");
 
-					$object['merchantId'] = $transaction->programID;
+					$transaction['custom_id'] = $item['click_reference'] ?? '';
 
-					$transactionDate = new \DateTime($transaction->date);
-					$object['date'] = $transactionDate->format("Y-m-d H:i:s");
+					$transaction['amount'] = round($item['value']['amount'] / 10000, 2);
+					$transaction['commission'] = round($item['commission']['amount'] / 10000, 2);
 
-					if ($transaction->clickRef != null) {
-						$object['custom_id'] = $transaction->clickRef;
+					if (in_array($item['status'], ['10', '20', '30', '40'])) {
+						$transaction ['status'] = \Oara\Utilities::STATUS_CONFIRMED;
+					} else if ($item['status'] == '70') {
+						$transaction ['status'] = \Oara\Utilities::STATUS_DECLINED;
+					} else {
+						$transaction ['status'] = \Oara\Utilities::STATUS_PENDING;
 					}
-
-					if ($transaction->status == 'confirmed' || $transaction->status == 'accepted') {
-						$object['status'] = \Oara\Utilities::STATUS_CONFIRMED;
-					} else
-						if ($transaction->status == 'delayed') {
-							$object['status'] = \Oara\Utilities::STATUS_PENDING;
-						} else
-							if ($transaction->status == 'cancelled' || $transaction->status == 'rejected') {
-								$object['status'] = \Oara\Utilities::STATUS_DECLINED;
-							}
-
-					$object['amount'] = \Oara\Utilities::parseDouble($transaction->saleValue);
-					$object['commission'] = \Oara\Utilities::parseDouble($transaction->commission);
-					$totalTransactions[] = $object;
+					$totalTransactions[] = $transaction;
 				}
 			}
-		}
+			$page++;
+
+		} while ($result['pagination']['current_page'] != $result['pagination']['last_page']);
 
 		return $totalTransactions;
+
 	}
+
+
+
+	private function apiCall($url, $post_data = null) {
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+		curl_setopt($ch, CURLOPT_HTTPHEADER, [
+			'Authorization: Bearer ' . $this->api_token,
+			'Content-Type: application/json;charset=UTF-8'
+		]);
+
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: Bearer " . $this->api_token));
+		if (!empty($post_data)) {
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+		}
+		$curl_results = curl_exec($ch);
+
+		$response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+		curl_close($ch);
+
+		try {
+			return json_decode($curl_results, true);
+		} catch (\Exception $e) {
+			throw new \Exception('Webgains fail: ' . $curl_results);
+		}
+
+	}
+
 
 
 }
